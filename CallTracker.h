86bac8df32ab8893ae24f589bcee4fb802409434
Sofia-sis-a <sofiakozlova23@gmail.com>
2026-06-08 13:05:06 +0300

    Update CallTracker.h

--- CallTracker.h
+++ CallTracker.h
@@ -5,21 +5,28 @@
 
 class CallTracker {
 private:
-    char* subscriberName;       
-    char* phoneNumber;          
-    double maxCallDuration;     
-    double minCallDuration;     
-    double totalDuration;       
-    int totalCalls;             
+    char* subscriberName;
+    char* phoneNumber;
+
+    double maxCallDuration = 0.0;
+    double minCallDuration = 0.0;
+    double totalDuration = 0.0;
+    int totalCalls = 0;
+
+    static constexpr double FREQUENT_THRESHOLD = 60.0;  // Именованная константа
 
 public:
     CallTracker(const char* name, const char* phone);
     ~CallTracker();
 
+    // Селекторы
+    const char* getName() const;
+    const char* getPhone() const;
     double getMaxDuration() const;
     double getMinDuration() const;
     double getTotalDuration() const;
     int getTotalCalls() const;
+
     double getAverageDuration() const;
     bool isFrequentCommunication() const;
 
